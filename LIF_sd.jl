3818f2929b9fe9834b9ad47351a65af8fc6e0319
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2023-10-03 18:28:13 -0500

    Tuned the key parameters

--- LIF_sd.jl
+++ LIF_sd.jl
@@ -77,7 +77,7 @@ function generate_spike_train(T, dt, initial_spike_time, tf)
     S_input[convert(Int, initial_spike_time/dt)] = 1
     
     # Calculate the time interval between spikes based on the temporal frequency
-    spike_interval = convert(Int, 1000/tf)
+    spike_interval = round(Int, 1000/tf)
     
     # Generate following spikes at evenly spaced intervals
     for i in 2:5  # since the first spike is already set, we generate the next 4 spikes
@@ -92,16 +92,16 @@ end
 
 # Example usage
 A = 20.0        # fixed parameter A
-d = 0.5         # depression fraction upon a spike
-f = 0.3         # facilitation increment upon a spike
-tau_d = 150.0     # time constant for D to recover to 1 (ms)
-tau_f = 150.0     # time constant for F to recover to 1 (ms)
+d = 0.515       # depression fraction upon a spike
+f = 0.085         # facilitation increment upon a spike
+tau_d = 50.0     # time constant for D to recover to 1 (ms)
+tau_f = 96.0     # time constant for F to recover to 1 (ms)
 dt = 1.0        # time step (ms)
-T = 600.0       # total time to simulate (ms)
+T = 3000.0       # total time to simulate (ms)
 
 # Generate spike train
 first_spike_time = 50.0  # ms
-temporal_frequency = 10.0  # Hz
+temporal_frequency = 50.0  # Hz
 S_input = generate_spike_train(T, dt, first_spike_time, temporal_frequency)
 
 # Run simulation
@@ -115,3 +115,36 @@ println("Depression ratio H_5/H_1: ", depression_ratio)
 # Plot results and save to file
 output_filename = "./figs/working.png"
 plot_LIF_simulation(time, Vs, spike_times, S_input, output_filename)
+
+using Plots
+
+# Example temporal_frequencies
+temporal_frequencies = [2,3,4,5,6,7,8,9,10,11,12, 13, 15, 20, 25, 30,35, 40, 45, 50]
+
+# Initialize an empty array to store results
+depression_ratios = []
+
+# Loop over all temporal_frequencies
+for tf in temporal_frequencies
+    global S_input, time, Vs, spike_times, Hs, depression_ratio
+    # Example: Generate spike train using the previously defined function
+    S_input = generate_spike_train(T, dt, first_spike_time, tf)
+
+    # Example: Run simulation and store depression ratio
+    time, Vs, spike_times, Hs = simulate_LIF_neuron(A, d, f, tau_d, tau_f, dt, T, S_input)
+    
+    # Ensure that we have recorded the required Hs values to calculate the depression ratio
+    if length(Hs) >= 2
+        depression_ratio = Hs[2] / Hs[1]
+        push!(depression_ratios, depression_ratio)
+    else
+        println("Warning: Not enough Hs values recorded for tf = $tf. Skipping.")
+        continue  # skip to the next iteration of the loop
+    end
+end
+
+# Plot results
+plot(temporal_frequencies, depression_ratios, xlabel="Temporal Frequency (Hz)", ylabel="Depression Ratio", label="Depression Ratio vs Temporal Frequency", marker=:circle, linewidth=2, linecolor=:blue)
+
+# Save figure
+savefig("depression_ratio_vs_temporal_frequency.png")

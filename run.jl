4ccbab8bb16ead892644843a0eb08f98817a18fa
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2023-10-17 14:30:01 -0500

    update_s

--- run.jl
+++ run.jl
@@ -39,16 +39,15 @@ jii = -16. / (taui*sqrtK)      #strength from I to I
 
 #times, ns, Ne, Ncells, T = sim(K,Ne,Ni,T,jie,jei,jii,jee)
 
-#println("mean excitatory firing rate: ", mean(1000 * ns[1:Ne] / T), " Hz")
-#println("mean inhibitory firing rate: ", mean(1000 * ns[(Ne+1):Ncells] / T), " Hz")
+
 
 #params = NetworkParameters(800, 4000, 1000, 1500, sqrt(800), 10, 15, 0.2 / (15 * sqrt(800)), 0.4 / (10 * sqrt(800)), -0.8 * 1.2 / (10 * sqrt(800)), -0.8 / (15 * sqrt(800)))
 params = NetworkParameters(K, Ne, Ni, T, sqrtK, taue, taui, jie, jee, jei, jii)
 #times, ns, Ne, Ncells, T = sim(params.K, params.Ne, params.Ni, params.T, params.jie, params.jei, params.jii, params.jee)
-times, ns, Ne, Ncells, T = sim_old()
+times, ns, Ne, Ncells, T, v_history, E_input, I_input = sim_old()
+
 println("mean excitatory firing rate: ", mean(1000 * ns[1:params.Ne] / params.T), " Hz")
 println("mean inhibitory firing rate: ", mean(1000 * ns[(params.Ne+1):Ncells] / params.T), " Hz")
-
 
 function compute_sliding_rate(spiketimes, window_size, step_size, T)
     n_neurons, _ = size(spiketimes)
@@ -83,24 +82,24 @@ if doplot
     plot_size = (600, 400) 
 
  # Parameters for sliding window
-window_size = 100  # in ms
-step_size = 5     # in ms
+   window_size = 100  # in ms
+   step_size = 5     # in ms
 
-e_rate = compute_sliding_rate(times[1:Ne, :], window_size, step_size, T)
-i_rate = compute_sliding_rate(times[(Ne+1):Ncells, :], window_size, step_size, T)
+   e_rate = compute_sliding_rate(times[1:Ne, :], window_size, step_size, T)
+   i_rate = compute_sliding_rate(times[(Ne+1):Ncells, :], window_size, step_size, T)
 
 # Compute the time values based on window_size and step_size
-n_steps = length(e_rate)  # or length(i_rate), assuming they have the same length
-time_values = [i * step_size + (window_size / 2) for i in 1:n_steps]
+   n_steps = length(e_rate)  # or length(i_rate), assuming they have the same length
+   time_values = [i * step_size + (window_size / 2) for i in 1:n_steps]
 
-p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)",
+   p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)",
           label="Excitatory", lw=2, linecolor=:red, size=plot_size)
 plot!(time_values, i_rate, label="Inhibitory", lw=2, linecolor=:deepskyblue2)
 
-fig_filename = "./figs/$timestamp_str.png"
+   fig_filename = "./figs/$timestamp_str.png"
 
 # Save the figure with the timestamped filename
-savefig(p2, fig_filename)
+   savefig(p2, fig_filename)
 
 println("Figure saved as $fig_filename")
     
@@ -108,140 +107,63 @@ println("Figure saved as $fig_filename")
 
     # Define filenames for the figure and JSON file
     
-    json_filename = "./figs/$timestamp_str.json"
+json_filename = "./figs/$timestamp_str.json"
 
-    # Save the figure with the timestamped filename
-   
-    # Store the parameters in a JSON file with the timestamped filename
-    param_dict = Dict(
-        "K" => params.K,
-        "Ne" => params.Ne,
-        "Ni" => params.Ni,
-        "T" => params.T,
-        "sqrtK" => params.sqrtK,
-        "taue" => params.taue,
-        "taui" => params.taui,
-        "jie" => params.jie,
-        "jee" => params.jee,
-        "jei" => params.jei,
-        "jii" => params.jii
-    )
+param_dict = Dict(
+    "K" => params.K,
+    "Ne" => params.Ne,
+    "Ni" => params.Ni,
+    "T" => params.T,
+    "sqrtK" => params.sqrtK,
+    "taue" => params.taue,
+    "taui" => params.taui,
+    "jie" => params.jie,
+    "jee" => params.jee,
+    "jei" => params.jei,
+    "jii" => params.jii
+)
 
-    JSON3.open(json_filename, "w") do io
-        JSON3.print(io, param_dict)
-    end
-
-    println("Parameters saved as $json_filename")
+   JSON3.open(json_filename, "w") do io
+    JSON3.print(io, param_dict)
 end
 
+println("Parameters saved as $json_filename")
+
+avg_correlation_E_I=compute_correlation(E_input, I_input)
+avg_correlation_E_E=compute_correlation(E_input, E_input)
+avg_correlation_I_I=compute_correlation(I_input, I_input)
+cross_corr_E_E=compute_cross_correlation(E_input[:, end-999:end], E_input[:, end-999:end])
+cross_corr_I_I=compute_cross_correlation(I_input[:, end-999:end], I_input[:, end-999:end])
+cross_corr_E_I=compute_cross_correlation(E_input[:, end-999:end], I_input[:, end-999:end])
+cross_corr_I_E=compute_cross_correlation(I_input[:, end-999:end], E_input[:, end-999:end])
+println("avg correlation(E-I): ", avg_correlation_E_I)
+println("avg correlation(E-E): ", avg_correlation_E_E)
+println("avg correlation(I-I): ", avg_correlation_I_I)
+#println("cross E-E: ", cross_corr_E_E)
 
 
+function plot_correlations(cross_corr_E_E, cross_corr_I_I, cross_corr_E_I,cross_corr_I_E)
+    # Assuming the dictionaries have the same keys
+    sorted_keys = sort(collect(keys(cross_corr_E_E)))
+    
+    sorted_values_E_E = [cross_corr_E_E[k] for k in sorted_keys]
+    sorted_values_I_I = [cross_corr_I_I[k] for k in sorted_keys]
+    sorted_values_E_I = [cross_corr_E_I[k] for k in sorted_keys]
+    sorted_values_I_E = [cross_corr_I_E[k] for k in sorted_keys]
 
-function run_simulation_and_tune_params(K, Ne, Ni, T, jie, jee, jei, jii)
-    params = NetworkParameters(K, Ne, Ni, T, sqrt(K), 10, 15, jie, jee, jei, jii)
+    plot(sorted_keys, sorted_values_E_E, label="E-E", linewidth=2, marker=:circle, color=:blue, legend=:topright)
+    plot!(sorted_keys, sorted_values_I_I, label="I-I", linewidth=2, marker=:circle, color=:red)
+    plot!(sorted_keys, sorted_values_E_I, label="E-I", linewidth=2, marker=:circle, color=:yellow)
+    plot!(sorted_keys, sorted_values_I_E, label="I-E", linewidth=2, marker=:circle, color=:orange)
     
-    times, ns, Ne, Ncells, T = sim(params.K, params.Ne, params.Ni, params.T, params.jie, params.jei, params.jii, params.jee)
-    
-    excitatory_rate = mean(1000 * ns[1:params.Ne] / params.T)
-    inhibitory_rate = mean(1000 * ns[(params.Ne + 1):Ncells] / params.T)
-    
-    return times, excitatory_rate, inhibitory_rate
+    xlabel!("Tau")
+    ylabel!("Correlation")
+    title!("Cross-correlation")
+    savefig("cross_correlation_plot.png")
+    display(plot)
 end
 
+plot_correlations(cross_corr_E_E, cross_corr_I_I, cross_corr_E_I,cross_corr_I_E)
 
-function tune_parameters()
-    doplot = true
- 
-    successful_adjustments = 0
-    required_successful_adjustments = 50
-    # Initial parameter values
-    K = 800
-    Ne = 4000
-    Ni = 1000
-    T = 1500
-    sqrtK = sqrt(K)
-    taue = 10
-    taui = 15
 
-    jie = 0.2 / (taui * sqrtK)
-    jee = 0.4 / (taue * sqrtK)
-    jei = -0.8 * 1.2 / (taue * sqrtK)
-    jii = -0.8 / (taui * sqrtK)
-    
-    adjustment_step = 0.001  # Adjust step size based on sensitivity
-    random_factor = 0.001   # Introduce randomness
-    while successful_adjustments < required_successful_adjustments
-     while true
-        println("Round+1")
-        times, excitatory_rate, inhibitory_rate = run_simulation_and_tune_params(K, Ne, Ni, T, jie, jee, jei, jii)
-
-        if excitatory_rate <= 5 && inhibitory_rate <= 5 && inhibitory_rate < excitatory_rate && inhibitory_rate >=0.5
-            println("Parameters tuned successfully:")
-            println("jie = $jie, jee = $jee, jei = $jei, jii = $jii")
-            println("Mean excitatory firing rate: $excitatory_rate Hz")
-            println("Mean inhibitory firing rate: $inhibitory_rate Hz")
-            successful_adjustments=successful_adjustments+1
-            if doplot
-                timestamp = Dates.now()
-                timestamp_str = Dates.format(timestamp, "yyyy-mm-dd_HH-MM-SS")
-
-                plot_size = (600, 400)
-
-                window_size = 100
-                step_size = 5
-
-                e_rate = compute_sliding_rate(times[1:Ne, :], window_size, step_size, T)
-                i_rate = compute_sliding_rate(times[(Ne + 1):Ncells, :], window_size, step_size, T)
-
-                n_steps = length(e_rate)
-                time_values = [i * step_size + (window_size / 2) for i in 1:n_steps]
-
-                p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)",
-                          label="Excitatory", lw=2, linecolor=:red, size=plot_size)
-                plot!(time_values, i_rate, label="Inhibitory", lw=2, linecolor=:skyblue)
-
-                fig_filename = "./figs/ss_$timestamp_str.png"
-                savefig(p2, fig_filename)
-                println("Figure saved as $fig_filename")
-
-                json_filename = "./figs/ss_$timestamp_str.json"
-                param_dict = Dict(
-                    "K" => K,
-                    "Ne" => Ne,
-                    "Ni" => Ni,
-                    "T" => T,
-                    "sqrtK" => sqrtK,
-                    "taue" => taue,
-                    "taui" => taui,
-                    "jie" => jie,
-                    "jee" => jee,
-                    "jei" => jei,
-                    "jii" => jii
-                )
-                JSON3.open(json_filename, "w") do io
-                    JSON3.print(io, param_dict)
-                end
-                println("Parameters saved as $json_filename")
-            end
-        
-
-        else
-            # Randomly adjust parameters with noise
-            jie += adjustment_step * (-1 + 2*random_factor * randn())
-            jee += adjustment_step * (-1 + 2*random_factor * randn())
-            jei -= adjustment_step * (-1 + 2*random_factor * randn())
-            jii -= adjustment_step * (-1 + 2*random_factor * randn())
-
-            jie = max(0.0001, jie)
-            jee = max(0.0001, jee)
-    
-            # Ensure jei and jii are less than 0
-            jei = min(-0.0001, jei)
-            jii = min(-0.0001, jii)
-        end
-    end
- end
 end
-
-
-#tune_parameters()
--- sim.jl
+++ sim.jl
@@ -1,6 +1,8 @@
 #this file is part of litwin-kumar_doiron_cluster_2012
 #Copyright (C) 2014 Ashok Litwin-Kumar
 #see README for more information
+using Statistics
+
 function sim_old()
     println("Setting up parameters")
 
@@ -8,7 +10,7 @@ function sim_old()
     Ncells = 5000
     Ne = 4000
     Ni = 1000
-    T = 1000  # Simulation time (ms)
+    T = 2000  # Simulation time (ms)
 
     taue = 15  # Membrane time constant for excitatory neurons (ms)
     taui = 10 # Membrane time constant for I neurons (ms)
@@ -31,8 +33,8 @@ function sim_old()
 
     # Stimulation
     Nstim = 400
-    stimstr = 0 #0.7 / taue
-    stimstart = T - 1500
+    stimstr = 0.7 / taue
+    stimstart = T - 1100
     stimend = T
 
     # Constants and thresholds
@@ -84,7 +86,10 @@ function sim_old()
     maxTimes = round(Int, maxrate * T / 1000)
     times = zeros(Ncells, maxTimes)
     ns = zeros(Int, Ncells)
-
+    Nsteps = round(Int, T / dt)
+    v_history = zeros(Ncells, Nsteps)  # Nsteps because we're saving at each time step, not just spikes
+    E_input=zeros(Ncells, Nsteps) 
+    I_input=zeros(Ncells, Nsteps)
     forwardInputsE = zeros(Ncells)
     forwardInputsI = zeros(Ncells)
     forwardInputsEPrev = zeros(Ncells)
@@ -99,7 +104,7 @@ function sim_old()
     #v = vre*ones(Ncells)
     lastSpike = -100 * ones(Ncells)
 
-    Nsteps = round(Int, T / dt)
+    
     println("Starting simulation")
 
     for ti = 1:Nsteps
@@ -113,7 +118,12 @@ function sim_old()
             xirise[ci] += -dt * xirise[ci] / taurise + forwardInputsIPrev[ci]
             xidecay[ci] += -dt * xidecay[ci] / tauidecay + forwardInputsIPrev[ci]
 
-            synInput = (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise) + (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
+            #synInput = (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise) + (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
+            synInput_E = (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise)
+            synIput_I = (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
+            E_input[ci, ti] = synInput_E
+            I_input[ci, ti] = synIput_I 
+            synInput = synInput_E+synIput_I # (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise) + (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
 
             if (ci < Nstim) && (t > stimstart) && (t < stimend)
                 synInput += stimstr
@@ -125,6 +135,7 @@ function sim_old()
        
                 if v[ci] > thresh[ci]
                     v[ci] = vre
+                    v_history[ci, ti] = v[ci]
                     lastSpike[ci] = t
                     ns[ci] += 1
                     if ns[ci] <= maxTimes
@@ -155,8 +166,101 @@ function sim_old()
        println("triger")
     end
 
-    return times, ns, Ne, Ncells, T
+    return times, ns, Ne, Ncells, T, v_history, E_input, I_input
 end
+
+
+
+
+
+
+function compute_correlation(E_input::Matrix, I_input::Matrix, num_pairs::Int=100)
+    # Ensure input matrices have the same dimensions
+    if size(E_input) != size(I_input)
+        error("E_input and I_input must have the same dimensions.")
+    end
+
+    Ncells, Nsteps = size(E_input)
+
+    # Ensure the number of pairs is less than or equal to Ncells
+    if num_pairs > Ncells
+        error("The number of pairs cannot exceed the number of cells.")
+    end
+
+    # Randomly select indices for both E_input and I_input
+    random_e_indices = rand(1:Ncells, num_pairs)
+    random_i_indices = rand(1:Ncells, num_pairs)
+
+    total_correlation = 0.0
+
+    for i = 1:num_pairs
+        e_data = E_input[random_e_indices[i], :]
+        i_data = I_input[random_i_indices[i], :]
+
+        # Compute Pearson correlation and add to the total
+        total_correlation += cor(e_data, i_data)
+    end
+
+    # Compute average correlation
+    avg_correlation = total_correlation / num_pairs
+
+    return avg_correlation
+end
+
+
+
+
+
+function compute_cross_correlation(E_input::Matrix, I_input::Matrix, tau_range::UnitRange{Int}=-5:5, num_pairs::Int=1000)
+    # Ensure input matrices have the same dimensions
+    if size(E_input) != size(I_input)
+        error("E_input and I_input must have the same dimensions.")
+    end
+
+    Ncells, Nsteps = size(E_input)
+
+    # Ensure the number of pairs is less than or equal to Ncells
+    if num_pairs > Ncells
+        error("The number of pairs cannot exceed the number of cells.")
+    end
+
+    # Randomly select indices for both E_input and I_input
+    random_e_indices = rand(1:Ncells, num_pairs)
+    random_i_indices = rand(1:Ncells, num_pairs)
+
+    avg_correlations = Dict{Int, Float64}()
+
+    for tau in tau_range
+        total_correlation = 0.0
+        for i = 1:num_pairs
+            e_data = E_input[random_e_indices[i], :]
+            i_data = I_input[random_i_indices[i], :]
+            
+            # Shift data according to tau
+            if tau > 0
+                e_data = e_data[1:end-tau*10]
+                i_data = i_data[tau*10+1:end]
+            elseif tau < 0
+                e_data = e_data[-tau*10+1:end]
+                i_data = i_data[1:end+tau*10]
+            end
+            
+            # Compute Pearson correlation for this tau and add to the total
+            total_correlation += cor(e_data, i_data)
+        end
+
+        # Compute average correlation for this tau
+        avg_correlations[tau] = total_correlation / num_pairs
+    end
+
+    return avg_correlations
+end
+
+
+
+
+
+
 
 
 
@@ -289,8 +393,9 @@ function sim_new()
             xedecay[ci] += -dt * xedecay[ci] / tauedecay + forwardInputsEPrev[ci]
             xirise[ci] += -dt * xirise[ci] / taurise + forwardInputsIPrev[ci]
             xidecay[ci] += -dt * xidecay[ci] / tauidecay + forwardInputsIPrev[ci]
-
-            synInput = (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise) + (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
+            synInput_E = (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise)
+            synIput_I = (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
+            synInput = synInput_E+synIput_I # (xedecay[ci] - xerise[ci]) / (tauedecay - tauerise) + (xidecay[ci] - xirise[ci]) / (tauidecay - taurise)
 
             if (ci < Nstim) && (t > stimstart) && (t < stimend)
                 synInput += stimstr
@@ -518,3 +623,119 @@ function sim_new_new()
     return times, ns, Ne, Ncells, T
 end
               
+
+
+
+
+
+
+
+
+function run_simulation_and_tune_params(K, Ne, Ni, T, jie, jee, jei, jii)
+params = NetworkParameters(K, Ne, Ni, T, sqrt(K), 10, 15, jie, jee, jei, jii)
+
+times, ns, Ne, Ncells, T = sim(params.K, params.Ne, params.Ni, params.T, params.jie, params.jei, params.jii, params.jee)
+
+excitatory_rate = mean(1000 * ns[1:params.Ne] / params.T)
+inhibitory_rate = mean(1000 * ns[(params.Ne + 1):Ncells] / params.T)
+
+return times, excitatory_rate, inhibitory_rate
+end
+
+
+function tune_parameters()
+doplot = true
+
+successful_adjustments = 0
+required_successful_adjustments = 50
+# Initial parameter values
+K = 800
+Ne = 4000
+Ni = 1000
+T = 1500
+sqrtK = sqrt(K)
+taue = 10
+taui = 15
+
+jie = 0.2 / (taui * sqrtK)
+jee = 0.4 / (taue * sqrtK)
+jei = -0.8 * 1.2 / (taue * sqrtK)
+jii = -0.8 / (taui * sqrtK)
+
+adjustment_step = 0.001  # Adjust step size based on sensitivity
+random_factor = 0.001   # Introduce randomness
+while successful_adjustments < required_successful_adjustments
+ while true
+    println("Round+1")
+    times, excitatory_rate, inhibitory_rate = run_simulation_and_tune_params(K, Ne, Ni, T, jie, jee, jei, jii)
+
+    if excitatory_rate <= 5 && inhibitory_rate <= 5 && inhibitory_rate < excitatory_rate && inhibitory_rate >=0.5
+        println("Parameters tuned successfully:")
+        println("jie = $jie, jee = $jee, jei = $jei, jii = $jii")
+        println("Mean excitatory firing rate: $excitatory_rate Hz")
+        println("Mean inhibitory firing rate: $inhibitory_rate Hz")
+        successful_adjustments=successful_adjustments+1
+        if doplot
+            timestamp = Dates.now()
+            timestamp_str = Dates.format(timestamp, "yyyy-mm-dd_HH-MM-SS")
+
+            plot_size = (600, 400)
+
+            window_size = 100
+            step_size = 5
+
+            e_rate = compute_sliding_rate(times[1:Ne, :], window_size, step_size, T)
+            i_rate = compute_sliding_rate(times[(Ne + 1):Ncells, :], window_size, step_size, T)
+
+            n_steps = length(e_rate)
+            time_values = [i * step_size + (window_size / 2) for i in 1:n_steps]
+
+            p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)",
+                      label="Excitatory", lw=2, linecolor=:red, size=plot_size)
+            plot!(time_values, i_rate, label="Inhibitory", lw=2, linecolor=:skyblue)
+
+            fig_filename = "./figs/ss_$timestamp_str.png"
+            savefig(p2, fig_filename)
+            println("Figure saved as $fig_filename")
+
+            json_filename = "./figs/ss_$timestamp_str.json"
+            param_dict = Dict(
+                "K" => K,
+                "Ne" => Ne,
+                "Ni" => Ni,
+                "T" => T,
+                "sqrtK" => sqrtK,
+                "taue" => taue,
+                "taui" => taui,
+                "jie" => jie,
+                "jee" => jee,
+                "jei" => jei,
+                "jii" => jii
+            )
+            JSON3.open(json_filename, "w") do io
+                JSON3.print(io, param_dict)
+            end
+            println("Parameters saved as $json_filename")
+        end
+    
+
+    else
+        # Randomly adjust parameters with noise
+        jie += adjustment_step * (-1 + 2*random_factor * randn())
+        jee += adjustment_step * (-1 + 2*random_factor * randn())
+        jei -= adjustment_step * (-1 + 2*random_factor * randn())
+        jii -= adjustment_step * (-1 + 2*random_factor * randn())
+
+        jie = max(0.0001, jie)
+        jee = max(0.0001, jee)
+
+        # Ensure jei and jii are less than 0
+        jei = min(-0.0001, jei)
+        jii = min(-0.0001, jii)
+    end
+end
+end
+end
+
+
+#tune_parameters()

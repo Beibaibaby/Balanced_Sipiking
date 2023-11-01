949568891e167233cf17d3cd40073d51eba5b448
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2023-11-01 17:22:48 -0500

    Update run_inject_3.0.0.jl

--- run_inject_3.0.0.jl
+++ run_inject_3.0.0.jl
@@ -124,10 +124,10 @@ function run_experiment(;
                 # Add a code to detect low rate or not 
                 
                 if low_plot ##this para control whether focus on the zoom in low activity
-                    p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)", label="Excitatory", lw=2, linecolor=:red, size=plot_size, title="Firing rate (d=$d)", ylim=(0,5))
+                    p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firing rate (Hz)", label="Excitatory", lw=2, linecolor=:red, size=plot_size, title="Firing rate (d=$d), (f=$f)", ylim=(0,5))
                     plot!(time_values, i_rate, label="Inhibitory", lw=2, linecolor=:deepskyblue2)
                 else
-                    p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firinçg rate (Hz)", label="Excitatory", lw=2, linecolor=:red, size=plot_size, title="Firing rate (d=$d)")
+                    p2 = plot(time_values, e_rate, xlabel="Time (ms)", ylabel="Firinçg rate (Hz)", label="Excitatory", lw=2, linecolor=:red, size=plot_size, title="Firing rate (d=$d), (f=$f)")
                     plot!(time_values, i_rate, label="Inhibitory", lw=2, linecolor=:deepskyblue2)
                 end 
                 
@@ -174,7 +174,7 @@ function run_experiment(;
                     label="Product over time", 
                     xlabel="Time (ms)", 
                     ylabel="Product Value", 
-                    title="Product of D and F (d=$d)", 
+                    title="Product of D and F (d=$d), (f=$f)", 
                     titlefontsize=title_fontsize,
                     size=(fig_width, fig_height)
                 )
@@ -184,7 +184,7 @@ function run_experiment(;
                     label="IE Mean History", 
                     xlabel="Time (ms)", 
                     ylabel="Value", 
-                    title="E->I strength (d=$d) ", 
+                    title="E->I strength (d=$d), (f=$f)", 
                     titlefontsize=title_fontsize,
                     size=(fig_width, fig_height)
                 )

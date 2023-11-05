bcb83caa19477d0c6a7ffb92f43c7730bac28df2
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2023-11-05 15:41:54 -0600

    Update exp.sh

--- exp.sh
+++ exp.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
 # Array of 'd' values
-declare -a d_values=(0.01 0.10)
+d_values=(0.05 0.10)
 
 # Array of 'f' values
-declare -a f_values=(0.05 0.1 0.2 0.3 0.40)
+f_values=(0.05 0.1 0.2 0.3)
 
-# Loop over 'd' values
+# Keep track of the number of cores
+num_cores=$(nproc)
+running_jobs=0
+
 for d in "${d_values[@]}"
 do
-  # Loop over 'f' values
   for f in "${f_values[@]}"
   do
-    echo "Running experiment with d = $d and f = $f"
-    julia run_inject_3.0.0.jl --d $d --f $f
+    echo "Running experiment with d = $d and f = $f on background"
+    julia run_inject_3.0.0.jl --d $d --f $f --T 1000 --stim_start_time 500 --stim_duration 3 --stimstr 1.0 &
+
+    # Increment the count of running jobs
+    ((running_jobs++))
+
+    # If we've reached the number of cores, wait for all jobs to complete
+    if (( running_jobs == num_cores )); then
+      wait # Wait for all background jobs to finish
+      running_jobs=0 # Reset the running jobs counter
+      echo "waiting"
+    fi
   done
 done
+
+# Wait for the remaining jobs to finish, if any
+wait
 
 echo "All experiments finished!"

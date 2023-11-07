d34fe2122dc6b6d8df7d9a338868aa70ca9ae10d
Beibaibaby <56213541+Beibaibaby@users.noreply.github.com>
2023-11-07 16:13:51 -0600

    update exps

--- exp_02.sh
+++ exp_02.sh
@@ -7,8 +7,9 @@
 #SBATCH --time=01:30:30
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=100gb
-#SBATCH --output=jobname.%j.out
-#SBATCH --error=jobname.%j.err
+#SBATCH --output=jobname.%A_%a.out # %A is replaced by the job ID and %a by the array index
+#SBATCH --error=jobname.%A_%a.err
+#SBATCH --array=1-6%3 # Creates a job array with 6 tasks and a maximum of 3 running simultaneously
 
 # Fixed parameters
 f=0.98
@@ -18,8 +19,11 @@ stimstr=0.8
 stim_start_time=400
 stim_duration=5
 
-# Loop over d values
-for d in 0.01 0.05 0.10 0.2 0.3 0.4
-do
-    julia run_inject_3.0.0.jl --d $d --f $f --T $T --env $env --stimstr $stimstr --stim_start_time $stim_start_time --stim_duration $stim_duration
-done
+# Array of d values
+d_values=(0 0.01 0.05 0.10 0.2 0.3 0.4)
+
+# Use SLURM_ARRAY_TASK_ID to select the d value
+d=${d_values[$SLURM_ARRAY_TASK_ID]}
+
+# Run the Julia script with the selected d value
+julia run_inject_3.0.0.jl --d $d --f $f --T $T --env $env --stimstr $stimstr --stim_start_time $stim_start_time --stim_duration $stim_duration
